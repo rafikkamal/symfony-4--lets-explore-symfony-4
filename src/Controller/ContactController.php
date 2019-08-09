ded8ded753bae16a784fbef42d85e9185f73022c
rafikkamal <rafik.rkn@gmail.com>
2019-08-09 18:56:20 +0600

    Added styling to the form. Worked with the data retrieved after the form is submitted. The data is not stored in the database yet.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 use App\Form\ContactType;
 
@@ -12,9 +13,18 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact()
+    public function contact(Request $request)
     {
     	$form = $this->createForm(ContactType::class);
+    	$form->handleRequest($request);
+    	
+    	if ($form->isSubmitted() && $form->isValid()) {
+            $contactFormData = $form->getData();
+            dump($contactFormData);
+            // Submit an email
+            
+        }
+
         return $this->render('contact/index.html.twig', [
             'our_form' => $form->createView(),
         ]);
