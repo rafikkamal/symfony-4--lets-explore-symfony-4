bc219359dd755c928395cd18b6678fea9ee61b79
rafikkamal <rafik.rkn@gmail.com>
2019-08-11 16:46:11 +0600

    Sent email. Worked with flash message.

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,16 +13,35 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(Request $request)
+    public function contact(Request $request, \Swift_Mailer $mailer)
     {
     	$form = $this->createForm(ContactType::class);
     	$form->handleRequest($request);
+
+    	//$this->addFlash('info', 'Some useful info');
     	
     	if ($form->isSubmitted() && $form->isValid()) {
             $contactFormData = $form->getData();
             dump($contactFormData);
-            // Submit an email
-            
+            /**
+             * Submit an email
+             */
+            $message = (new \Swift_Message('Hello Email'))
+		        ->setFrom($contactFormData['from'])
+		        ->setTo('rafik.rkn@gmail.com')
+		        ->setBody(
+		            $contactFormData['message'],
+		            'text/plain'
+		        );
+		    /**
+		     * Send email 
+		     */
+		    if($mailer->send($message)) {
+		    	$this->addFlash('success', 'Email was sent!');
+		    	return $this->redirectToRoute('contact');	
+		    } else {
+		    	$this->addFlash('alert', 'Failed to send email!');
+		    }
         }
 
         return $this->render('contact/index.html.twig', [
